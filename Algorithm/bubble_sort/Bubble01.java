ad13f4474ca85014f08ad89f3050316f52683c09
DESKTOP-F9CUFM9\user <freshkimdh@gmail.com>
2020-10-24 11:54:51 +0900

    버블정렬 - ver 1

---
+++ Algorithm/bubble_sort/Bubble01.java
@@ -0,0 +1,48 @@
+package bubble_sort;
+
+import java.util.Scanner;
+
+public class Bubble01 {
+	
+	//서로 값을 바꿈
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	//버블 정렬
+	static void bubbleSort(int[] a, int n) {
+		for(int i =0; i<n-1; i++) {
+			for(int j = n-1; j>i; j--) {
+				if(a[j - 1] > a[j]) {
+					swap(a, j-1, j);
+				}
+			}//j for 문
+		}// i for 문
+	}//bubleSort 함수
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("버블 정렬 ver_1");
+		System.out.print("요솟수: ");
+		int nx = sc.nextInt();
+		int[] x = new int[nx];
+		
+		for(int i=0; i<nx; i++) {
+			System.out.print("x[" + i + "]:");
+			x[i]=sc.nextInt();
+		}
+		
+		bubbleSort(x, nx);
+		
+		System.out.println("오름차순으로 정렬 했습니다.");
+		for(int i=0; i<nx; i++) {
+			System.out.println("x[" + i + "]=" + x[i]);
+		}
+	}
+	
+	
+}
